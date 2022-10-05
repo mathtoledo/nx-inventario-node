61cb3d9b8853674d801ff1eb0f7d4038815b3ed7
Matheus Toledo <ttoledomatheus@gmail.com>
2022-10-05 09:47:21 -0300

    feat: Adiciona middleware para tratar erros

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
 import { routes } from './routes'
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) return res.status(400).json({ message: err.message })
+  return res.status(500).json({ message: "Erro interno de servidor" })
+})
 
 app.listen(3333, () => '🚀 NX Inventario server está rodando')
